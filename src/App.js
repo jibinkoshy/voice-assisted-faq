3e0cec407df3e4b883c7ceedd57afb52ffd1c289
jibinkoshy <jibinkoshy@hotmail.com>
2021-01-12 22:07:32 -0600

    Add Faq component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { ChakraProvider, theme } from '@chakra-ui/react';
 import { Navbar } from './Navbar';
+import { Faq } from './faq';
+import { FaqPage } from './FaqPage';
 
 export const App = () => {
   return (
     <ChakraProvider theme={theme}>
       <Navbar />
+      <FaqPage />
     </ChakraProvider>
   );
 };
