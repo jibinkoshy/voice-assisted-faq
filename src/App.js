9f99eb54222994de318bde162237de9c62adc197
jibinkoshy <jibinkoshy@hotmail.com>
2021-01-12 22:58:47 -0600

    Add react-scroller

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { ChakraProvider, theme } from '@chakra-ui/react';
 import { Navbar } from './Navbar';
 import { FaqPage } from './FaqPage';
 import alanBtn from '@alan-ai/alan-sdk-web';
+import { scroller } from 'react-scroll';
 
 export const App = () => {
   const alanBtnInstance = useRef(null);
@@ -16,6 +17,11 @@ export const App = () => {
           '244691f9b0411585c1a56590f07475162e956eca572e1d8b807a3e2338fdd0dc/stage',
         onCommand: commandData => {
           if (commandData.command === 'gotoFaq') {
+            scroller.scrollTo(`accordion-item-${commandData.faqId}`, {
+              duration: 800,
+              delay: 0,
+              smooth: 'easeInOutQuart',
+            });
             setIndex(commandData.faqId - 1);
           }
         },
